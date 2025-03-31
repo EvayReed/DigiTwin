5349a67aec924fd7eef606faf83ea85bf221a698
YiweiDing-aboci <yiweiding@sending.tech>
2025-03-31 08:42:27 +0800

    update user token

--- app/core/utils/user.py
+++ app/core/utils/user.py
@@ -29,11 +29,27 @@ def get_user_by_id(user_id: int) -> User | None:
         return user
 
 
+def update_user_token(user_id: int, token: str) -> User:
+    with next(get_db()) as db:
+        user = db.query(User).filter(User.userId == user_id).first()
+        if user:
+            user.token = token
+            user.name = "dingkai开饿了"
+            db.commit()
+            db.refresh(user)
+            return user
+        else:
+            return None
+
+
 def get_user_config(user_id: int, name: str, avatar: str = None, email: str = None, token: str = None) -> dict:
     user = get_user_by_id(user_id)
 
     if not user:
         user = create_user(user_id, name, avatar, email, token)
+    else:
+        if user.token != token:
+            user = update_user_token(user_id, token)
 
     agents = get_records_by_user_id(user_id)
 
@@ -45,14 +61,3 @@ def get_user_config(user_id: int, name: str, avatar: str = None, email: str = No
         "token": user.token,
         "agents": agents
     }
-
-
-user_config = get_user_config(
-    user_id=343452,
-    name="name",
-    avatar="picture",
-    email="email",
-    token="9847334t590293r792q"
-)
-
-print(user_config)
