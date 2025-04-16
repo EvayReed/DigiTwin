1bba72866db001b9c0b8d9579956f1164e20da0e
YiweiDing-aboci <yiweiding@sending.tech>
2025-04-16 17:31:07 +0800

    update chart

--- app/core/utils/chat.py
+++ app/core/utils/chat.py
@@ -1,10 +1,10 @@
 import logging
 from datetime import datetime
 from typing import List
-from langchain.schema import BaseMessage, HumanMessage, AIMessage
+from langchain.schema import BaseMessage, HumanMessage, AIMessage, SystemMessage
 from app.core.models.chat import ChatMessage
 from app.core.prompts.credentials import credentials_tool_dict
-from app.core.prompts.system import HINT
+from app.core.prompts.system import HINT, main_prompt
 from app.core.tools.prompt_route import PromptRouterChain
 from app.services.database_service import get_db
 from app.services.llm_service import ai_engine
@@ -31,10 +31,8 @@ def chat(query: str, chat_room_id: str) -> str:
                     history.append(AIMessage(content=msg.content))
 
             response = generateResponse(history, query)
-            print(response)
-
             history.append(HumanMessage(content=query))
-            history.append(AIMessage(content=response.content))
+            history.append(AIMessage(content=response.get("content")))
             history.append(HumanMessage(content=HINT))
             hint_result = llm(history)
             print(hint_result.content)
@@ -45,18 +43,19 @@ def chat(query: str, chat_room_id: str) -> str:
                 content=query
             )
             session.add(user_msg)
-
             ai_msg = ChatMessage(
                 chat_room_id=chat_room_id,
                 role="ai",
-                content=response.content
+                content=response.get("content")
+                # content=f"{response.get("refers")}{response.get("content")}"
             )
             session.add(ai_msg)
             session.commit()
 
             return {
-                "result": response.content,
-                "hint": hint_result.content
+                "result": response.get("content"),
+                "hint": hint_result.content,
+                "object": response
             }
 
         except Exception as e:
@@ -71,15 +70,33 @@ class ToolResponse:
         self.content = content
 
 
+def append_context(context_texts=None):
+    if not context_texts:
+        return ""
+
+    result = "\nContext:\n"
+    for i, text in enumerate(context_texts):
+        result += f"[CONTEXT {i}]:\n{text}\n[END CONTEXT {i}]\n\n"
+    return result
+
+
 def generateMonthlyLedger(history: List[BaseMessage], query: str, prompt: str) -> ToolResponse:
     llm = ai_engine.get_openai_model()
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    refers = ";".join(vector_db_man.query_knowledge_base(query, "sdm"))
+    refers = append_context(vector_db_man.query_knowledge_base(query, "sdm"))
     logging.error(f"{query}查询到的相关信息：{refers}")
-    history.append(HumanMessage(content=refers))
-    history.append(HumanMessage(content=prompt))
-    history.append(HumanMessage(content=f"{query}(此刻是{current_time})"))
-    return llm.invoke(history)
+    # messages = [SystemMessage(content=f"此刻是{current_time}{main_prompt}{refers}")]
+    messages = [SystemMessage(content=f"此刻是{current_time}{prompt}{refers}")]
+    messages.extend(history)
+    messages.append(HumanMessage(content=query))
+    # history.append(HumanMessage(content=refers))
+    # history.append(HumanMessage(content=prompt))
+    # history.append(HumanMessage(content=f"{query}(此刻是{current_time})"))
+    return {
+        "content":  llm.invoke(messages).content,
+        "refers": refers,
+        "history": history
+    }
 
 
 def generateResponse(history: List[BaseMessage], query: str) -> ToolResponse:
